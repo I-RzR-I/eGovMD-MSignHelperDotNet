79f188d6f29af08a555ccc25dfe40e39c43ce21a
RzR <ddpRzR@hotmail.com>
2023-07-05 09:06:05 +0300

    Update libary version

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,2 @@
+### **1.0.2.0557** 
+-> Update libary version.<br />
--- src/shared/GeneralAssemblyInfo.cs
+++ src/shared/GeneralAssemblyInfo.cs
@@ -41,6 +41,6 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
-[assembly: AssemblyVersion("1.0.1.1521")]
-[assembly: AssemblyFileVersion("1.0.1.1521")]
-[assembly: AssemblyInformationalVersion("1.0.1.*")]
+[assembly: AssemblyVersion("1.0.2.0557")]
+[assembly: AssemblyFileVersion("1.0.2.0557")]
+[assembly: AssemblyInformationalVersion("1.0.2.*")]
